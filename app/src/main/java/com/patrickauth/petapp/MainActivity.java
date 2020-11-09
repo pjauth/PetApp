2edb37d37b57d7012c53ee411a37cb6c8f6fb062
patrickauth <pjauth@loyola.edu>
2020-11-09 00:48:25 -0500

    Working on first page

--- app/src/main/java/com/patrickauth/petapp/MainActivity.java
+++ app/src/main/java/com/patrickauth/petapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.patrickauth.petapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton PawPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PawPrint.findViewById(R.id.Logo);
     }
 }
